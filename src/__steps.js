160cfa8751ea1164c8319eff7dc88b4ded3be193
Md.Lutfor Rahman <61869128+lutforrahmanseu@users.noreply.github.com>
2022-10-19 18:58:14 +0600

    layout setup

---
+++ src/__steps.js
@@ -0,0 +1,47 @@
+/*
+----------------------------------------------------------
+Firebase set up 
+1.create firebase app
+2.get firebase configuration inside firebase.config.js
+3.make sure export app from firebase config
+4.create a react app
+5.install firebase and react-router-dom install
+-----------------------------------------------------------
+React-router:
+1.react-router-install
+2.router setup
+
+------------------------------------------------------------
+component:
+
+
+1.tailwind css install
+2.tailwind component daisy ui
+
+or
+
+1.react bootstrap install 
+1.import 'bootstrap/dist/css/bootstrap.min.css'; //index.js fill import
+
+*/
+
+/*
+--------------------------------------------
+layout Create:main.js
+shared components:
+1.header
+2.left side
+3.right side
+4.footer
+--------------------------------------------
+*/
+
+/*
+--------------------------------------------
+Pages create :
+1.category
+2.home
+3.news
+--------------------------------------------
+
+*/
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCRFysb0_O-YWKwxQwTIYkmgCnHpYVlE6w",
+  authDomain: "dragon-news-8ff7c.firebaseapp.com",
+  projectId: "dragon-news-8ff7c",
+  storageBucket: "dragon-news-8ff7c.appspot.com",
+  messagingSenderId: "972042604278",
+  appId: "1:972042604278:web:ab1b90dd6f835f7eaa66d7",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
